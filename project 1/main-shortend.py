091021ec8bcd5fbe275680363a9a3955008d9b0c
Aashish-kumar01 <kumarashish69959@gmail.com>
2026-09-15 03:05:07 +0530

    Updated File

---
+++ project 1/main-shortend.py
@@ -0,0 +1,75 @@
+import random
+
+
+
+computer = random.choice([1,-1,0])
+youstr = input("Enter you choice: ")
+
+youDict = {"s":1, "w": -1, "g":0}
+reverseDict = {1:"snake", -1:"Water", 0:"gun"}
+you = youDict[youstr]
+
+print(f"You choose {reverseDict[you]}\nComputer choose {reverseDict[computer]}")
+if computer == you:
+    print("It's a Draw")
+else:
+    # if you == 1 and computer == -1: (you-computer=2)
+    #     print("You Win!")
+    # elif you == 1 and computer == 0: (you-computer=1)
+    #     print("You lose")
+    # elif you == -1 and computer == 0: (you-computer=-1)
+    #     print("You Win!")
+    # elif you == -1 and computer == 1: (you-computer=-2)
+    #     print("You lose")
+    # elif you == 0 and computer == 1: (you-computer=-1)
+    #     print("You Win!")
+    # elif you == 0 and computer == -1: (you-computer=1)
+    #     print("You lose")
+    # else:
+    #     print("Something went wrong!")
+    
+
+
+    if you-computer== -2 or 1:
+        print("You Won!")
+    else:
+        print("You lose")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
